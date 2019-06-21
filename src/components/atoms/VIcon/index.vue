65ecef8dee927ff445b1fefd7914521d3547f06a
Serhan Coşkun <grafikri@gmail.com>
2019-06-21 12:19:12 +0300

    Component of icons was created

---
+++ src/components/atoms/VIcon/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <span></span>
+</template>
+
+<style lang="sass" scoped>
+span
+  display: block
+  background-image: url($icons-link)
+  background-repeat: no-repeat
+  height: 24px
+  width: 24px
+
+  &.compass
+    background-position: -205px -200px
+  &.user
+    background-position: -130px -274px
+  &.fav
+    background-position: -286px -274px
+
+</style>
+
+
+
+
+
