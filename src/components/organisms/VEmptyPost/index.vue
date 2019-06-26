816a5c47ad2cbdff70d2e80be8d5edb4a5ccc0fa
Serhan Coşkun <grafikri@gmail.com>
2019-06-26 16:27:31 +0300

    empty post page was created

---
+++ src/components/organisms/VEmptyPost/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="v-o-empty-post">
+    <div class="left">
+      <VImage
+        width="100%"
+        url="https://www.instagram.com/static/images/mediaUpsell.jpg/6efc710a1d5a.jpg"
+      />
+    </div>
+    <div class="right">
+      <div class="desc">
+        <div>
+          <VText bold="bold" size="18px">Start capturing and sharing your moments.</VText>
+        </div>
+        <div>
+          <VText size="16px">Get the app to share your first photo or video.</VText>
+        </div>
+      </div>
+      <div class="buttons">
+        <div>
+          <VButton url="apple">
+            <VImage
+              height="40px"
+              url="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_english-en.png/180ae7a0bcf7.png"
+            />
+          </VButton>
+        </div>
+        <div>
+          <VButton url="android">
+            <VImage
+              height="40px"
+              url="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/e9cd846dc748.png"
+            />
+          </VButton>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import VImage from '@/components/atoms/VImage'
+import VButton from '@/components/atoms/VButton'
+import VText from '@/components/atoms/VText'
+
+export default {
+  name: "VEmptyPost",
+  components: {
+    VImage,
+    VButton,
+    VText,
+  },
+
+}
+</script>
+
+<style lang="sass" scoped>
+  .v-o-empty-post
+    display: flex
+    .left
+      flex-basis: 40%
+    .right
+      background: white
+      flex-grow: 1
+      display: flex
+      flex-direction: column
+      justify-content: center
+      .desc
+        display: flex
+        flex-direction: column
+        text-align: center
+        div
+          margin:
+            bottom: 15px
+      .buttons
+        display: flex
+        justify-content: center
+        div
+          margin:
+            left: 10px
+            right: 10px
+    
+</style>
