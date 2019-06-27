d6600c9f373fad7b1d3e0b877e0e61b8649c5c7d
Serhan Coşkun <grafikri@gmail.com>
2019-06-27 11:02:34 +0300

    VLink component was created

---
+++ src/components/atoms/VLink/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <component class="v-a-link" :to="to" :is="component" :href="url" @click="handleClick">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: "VLink",
+  methods: {
+    handleClick(event) {
+      this.$emit('click', event)
+    }
+  },
+  computed: {
+    component() {
+      if (this.to) return 'router-link'
+      if (this.url) return 'a'
+      return 'button'
+    }
+  },
+  props: {
+    to: [String, Object],
+    url: String
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+  .v-a-link
+    font-size: 14px
+    cursor: pointer
+    line-height: 0
+    display: inline-block
+    padding: 0
+    margin: 0
+    text-decoration: none
+    background: none
+    border: none
+    &:focus
+      outline: none
+</style>
