771e25f40d614c7659f87e7c8935ae939c002b33
Serhan Coşkun <grafikri@gmail.com>
2019-06-28 15:12:22 +0300

    image component is being tested

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,22 @@
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>-->
-    <router-view/>
+    <!-- <router-view/> -->
+    <div class="box" style="width: 400px; height: 400px;">
+      <VImage
+        width="250"
+        measureType="px"
+        aspectRatio="1:1"
+        url="https://fakeimg.pl/200x100/?text=Photo&font=lobster"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex"
 
+import VImage from '@/components/atoms/VImage'
 import style from './assets/style/_colors.sass'
 
 
@@ -18,7 +27,7 @@ import style from './assets/style/_colors.sass'
 export default {
   name: "App",
   components: {
-
+    VImage
   },
   created() {
     this.fetch()
