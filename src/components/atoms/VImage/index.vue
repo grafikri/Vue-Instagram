bcda6750f62b427410d8c1e02c05c7ec25be08bc
Serhan Coşkun <grafikri@gmail.com>
2019-06-28 11:34:59 +0300

    image box is being designed

--- src/components/atoms/VImage/index.vue
+++ src/components/atoms/VImage/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <img class="v-a-image" :style="styleObject" :src="url">
+  <div class="v-a-image" :style="containerStyle">
+    <!-- <img ref="photo" class="v-a-image" :style="styleObject" :src="url"> -->
+  </div>
+  <!-- <VImage aspectRatio="1" width="100px" /> -->
 </template>
 
 <script>
@@ -10,7 +13,19 @@ export default {
     width: [String],
     height: [String],
     rounded: Boolean,
-    aspectRatio: Boolean
+    aspectRatio: {
+      type: String,
+      default: "1:1"
+    },
+    measureType: {
+      type: String, //pixel or percent,
+      default: "%"
+    }
+  },
+  created() {
+  },
+  mounted() {
+    console.log("this: ", this.$el.parentNode.offsetWidth)
   },
   computed: {
     styleObject() {
@@ -19,35 +34,49 @@ export default {
 
       return {
         borderRadius: borderRadius,
-        ...this.calculateWidthHeight()
+        // ...this.calculateWidthHeight()
       }
-    }
-  },
-  methods: {
-    calculateWidthHeight() {
+    },
+    containerStyle() {
 
-      let width = this.width ? this.width : "initial"
-      let height = this.height ? this.height : "initial"
+      let boxWidth = "initial"
+      let boxHeight = "initial"
 
-      if (this.aspectRatio) {
-        if (this.width) {
-          height = "initial"
-        } else if (this.height) {
-          width = "initial"
-        }
+      if (this.width && this.height) {
+        boxWidth = this.width + this.measureType
+        boxHeight = this.height + this.measureType
+
+      } else if (this.width) {
+
+        boxWidth = this.width + this.measureType
+        boxHeight = (this.width / this.aspectRatioCons) + this.measureType
+      }
+      else if (this.height) {
+        boxHeight = this.height + this.measureType
+        boxWidth = (this.height / this.aspectRatioCons) + this.measureType
       }
 
       return {
-        width: width, height: height
+        width: boxWidth, height: boxHeight
       }
-
+    },
+    aspectRatioCons() {
+      let arrAspectRatio = this.aspectRatio.split(":")
+      let width = arrAspectRatio[0]
+      let height = arrAspectRatio[1]
+      return width / height
     }
   },
+  methods: {
+
+
+  }
 }
 </script>
 
 <style lang="sass" scoped>
-
+  .v-a-image
+    background: red
 </style>
 
 
