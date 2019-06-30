75a39bfc72775cecb1772b5115c7efb87a606bfd
Serhan Coşkun <grafikri@gmail.com>
2019-06-30 16:06:20 +0300

    posts data was linked to component

--- src/components/pages/VProfilePage/index.vue
+++ src/components/pages/VProfilePage/index.vue
@@ -16,13 +16,14 @@ export default {
   },
   created() {
     this.fetchUser({ id: this.$route.params.id })
+    this.fetchPosts({ id: this.$route.params.id })
   },
   methods: {
-    ...mapActions({ fetchUser: 'user/fetchUser' })
+    ...mapActions({ fetchUser: 'user/fetchUser', fetchPosts: 'posts/fetchPosts' })
   },
   computed: mapState({
     user: 'user',
-    'posts': state => state.posts.map((post) => {
+    'posts': state => state.posts.items.map((post) => {
       return {
         photo: post.photo.thumb,
         likeCount: post.amount.like,
