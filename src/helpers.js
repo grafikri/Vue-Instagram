5391333ef1a93530ba61bc78716222348b47b8b7
Serhan Coşkun <grafikri@gmail.com>
2019-06-30 21:15:47 +0300

    created a helper file for common methods

---
+++ src/helpers.js
@@ -0,0 +1,15 @@
+
+
+export const randomPage = (max = 20) => {
+  return Math.floor(Math.random() * max) + 1
+}
+
+/**
+ * This method creates fake image 
+ * 
+ * @param {fake image width} width 
+ * @param {fake image height} height 
+ */
+export const getFakeImage = (width = 200, height = 200) => {
+  return "https://fakeimg.pl/" + width + "x" + height + "/?text=Photo&font=lobster"
+}
