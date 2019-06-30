404b0bf1c2fceb9b47d71a19a5544e07477104c5
Serhan Coşkun <grafikri@gmail.com>
2019-06-30 21:17:04 +0300

    auth user was fetched from server

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,22 +1,69 @@
+import theMovieDb from '@/service/theMovieDb'
+
 const state = {
-  name: "grafikri",
-  full_name: "Serhan",
-  photo: "https://fakeimg.pl/200x200/?text=Photo&font=lobster",
-  posts: {
-    count: 2,
-    list: []
-  },
-  followers: {
-    count: 5,
-    list: []
-  },
-  follow: {
-    count: 10,
-    list: []
-  },
+  auth: {
+    id: 6193,
+    name: "grafikri",
+    full_name: "Serhan",
+    photo: "https://fakeimg.pl/200x200/?text=Photo&font=lobster",
+    posts: {
+      count: 2,
+      list: []
+    },
+    followers: {
+      count: 5,
+      list: []
+    },
+    follow: {
+      count: 10,
+      list: []
+    }
+  }
 }
+
+const mutations = {
+  updateAuth(state, payload) {
+    state.auth = { ...state.auth, ...payload.user }
+
+  }
+}
+
+const getters = {
+  auth(state) {
+    return state.auth
+  }
+}
+
+
+
+const actions = {
+  fetchAuth(context, payload) {
+
+    let userId = 6193 // Leonardo DiCaprio ID
+
+    theMovieDb.people.getById({ id: userId }, success => {
+      let response = JSON.parse(success)
+      let user = {
+        id: response.id,
+        name: response.name,
+        full_name: response.name,
+        photo: theMovieDb.common.images_uri + "w154" + response.profile_path,
+      }
+      context.commit("updateAuth", { user })
+
+    }, fail => {
+
+    })
+
+
+  }
+}
+
 
 export default {
   namespaced: true,
-  state
+  actions,
+  mutations,
+  state,
+  getters
 }
