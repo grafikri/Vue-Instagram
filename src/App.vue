64e9e7ce740c31a440c1982223f35be409f9833d
Serhan Coşkun <grafikri@gmail.com>
2019-06-30 21:20:56 +0300

    unnecessary lines was deleted

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,13 @@
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>-->
-
-    <!-- url="https://fakeimg.pl/200x100/?text=Photo&font=lobster" -->
-    <!-- <div class="box" style="width: 300px; height: 200px">
-      <VImage
-        :width="180"
-        url="http://i.hurimg.com/i/hurriyet/75/300x170/5cf4caa22269a224b8e90d61.jpg"
-      />
-    </div>-->
-    <!-- <VBoxImage
-      :ratio="9/17"
-      url="http://image.tmdb.org/t/p/original/q0MPIW1uBsENV2WuW8VuEyZDioN.jpg"
-    />-->
-    <router-view/>
+    <router-view :key="$route.path"/>
   </div>
 </template>
 
 <script>
+
+
 import { mapState, mapActions } from "vuex"
 
 import VBoxImage from '@/components/atoms/VBoxImage'
@@ -39,10 +29,12 @@ export default {
     VPhotoCard
   },
   created() {
-    this.fetch()
+    this.fetchAuth()
+    this.fetchUsers()
+    this.fetchStories()
   },
   methods: {
-    ...mapActions({ fetch: 'timeline/fetchPopularPeople' })
+    ...mapActions({ fetchUsers: 'timeline/fetchPopularPeople', fetchStories: 'stories/fetchStories', fetchAuth: 'auth/fetchAuth' })
   },
 
 }
