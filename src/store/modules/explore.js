f4cccca5561f055da186a8058629164e5f49ec5e
Serhan Coşkun <grafikri@gmail.com>
2019-07-01 13:03:04 +0300

    real data was fetched from API

--- src/store/modules/explore.js
+++ src/store/modules/explore.js
@@ -1,67 +1,78 @@
-const state = [{
-  id: "1",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: "1000"
-}, {
-  id: "2",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: "1000"
-}, {
-  id: "3",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: "1000"
-}, {
-  id: "4",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: "1000"
-}, {
-  id: "5",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: 1000
-}, {
-  id: "6",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: 1000
-}, {
-  id: "6",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: 1000
-}, {
-  id: "6",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: 1000
-}, {
-  id: "6",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: 1000
-}, {
-  id: "6",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: 1000
-}, {
-  id: "6",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: 1000
-}, {
-  id: "6",
-  photo: "https://fakeimg.pl/500x500/?text=Photo&font=lobster",
-  commentCount: "3000",
-  likeCount: 1000
-}]
+import theMovieDb from '@/service/theMovieDb'
+import { randomNumber, getFakeImage, randomPage } from '@/helpers'
+
+
+const state = {
+  posts: [],
+  users: []
+}
+
+const mutations = {
+  updateAllPosts(state, payload) {
+    state.posts = []
+    state.posts = payload.list
+  },
+  updateAllUsers(state, payload) {
+    state.users = []
+    state.users = payload.list
+  },
+}
+
+const getters = {
+  fetchPosts(state) {
+    return state.posts.map(item => {
+      return {
+        id: item.id,
+        photo: item.profile_path ? theMovieDb.common.images_uri + "w500" + item.profile_path : getFakeImage(),
+        commentCount: randomNumber(10) + "k",
+        likeCount: randomNumber(300) + "k"
+      }
+    })
+  },
+  fetchUsers(state) {
+    return state.users.map(item => {
+      return {
+        id: item.id,
+        photo: item.profile_path ? theMovieDb.common.images_uri + "w500" + item.profile_path : getFakeImage(),
+        name: item.name,
+        desc: "Follows you"
+      }
+    })
+  },
+}
+
+const actions = {
+  fetchExplorePosts(context) {
+
+    theMovieDb.people.getPopular({ page: randomPage() }, success => {
+
+      let response = JSON.parse(success)
+      context.commit('updateAllPosts', { list: response.results })
+
+    }, fail => {
+
+    })
+
+  },
+  fetchExploreUsers(context) {
+
+    theMovieDb.people.getPopular({ page: randomPage() }, success => {
+
+      let response = JSON.parse(success)
+      context.commit('updateAllUsers', { list: response.results })
+
+    }, fail => {
+
+    })
+
+  },
+}
 
 
 export default {
   namespaced: true,
+  getters,
+  actions,
+  mutations,
   state
 }
