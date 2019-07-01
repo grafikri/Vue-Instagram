a13c1183eaaad552c0120b4898cf47fdd674b6a0
Serhan Coşkun <grafikri@gmail.com>
2019-07-01 23:29:59 +0300

    App loading page and templates was created

---
+++ src/components/templates/VAppLoading/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="v-t-app-loading">
+    <div class="container">
+      <VLogo />
+    </div>
+  </div>
+</template>
+
+<script>
+import VLogo from '@/components/atoms/VLogo'
+export default {
+  name: "VAppLoading",
+  components: {
+    VLogo,
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+  .v-t-app-loading
+    height: 100vh
+    display: flex
+    .container
+      margin: auto
+</style>
