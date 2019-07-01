8a69cb93f64252e1d328c70db83aeda0559e4c41
Serhan Coşkun <grafikri@gmail.com>
2019-07-01 13:00:57 +0300

    custom line support was added

--- src/components/atoms/VUserName/index.vue
+++ src/components/atoms/VUserName/index.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="v-a-user-name">
-    <p>
+    <p :style="textStyle" :class="lines ? 'lineCountActive': ''">
       <slot></slot>
     </p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "VUserName",
+  props: {
+    lines: {
+      type: [Number, Boolean],
+      default: false
+    },
+  },
+  computed: {
+    textStyle() {
+      return {
+        "webkitLineClamp": this.lines ? this.lines : "initial"
+      }
+    }
+  },
+}
+</script>
+
 
 <style lang="sass" scoped>
   .v-a-user-name
@@ -13,5 +33,8 @@
       font-size: 14px
       font-weight: 600
       margin: 0
+      &.lineCountActive
+        @include customDot(1)
+    
 </style>
 
