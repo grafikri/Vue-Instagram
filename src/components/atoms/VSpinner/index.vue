34f5b3e25469f6afc5ec030523316051916ee393
Serhan Coşkun <grafikri@gmail.com>
2019-07-05 15:10:07 +0300

    spinner component was created for page loading

---
+++ src/components/atoms/VSpinner/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="v-a-spinner">
+    <img src="@/assets/spinner.gif" />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+  .v-a-spinner
+    display: inline-block
+    width: 32px
+    height: 32px
+    img
+      width: 100%
+  
+    
+</style>
